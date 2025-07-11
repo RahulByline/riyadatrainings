84df03e7ec7c2b51d76aea4cf4a6295084075fe0
RahulByline <rahulkirad.byline@gmail.com>
2025-07-11 18:45:17 +0530

    Fix CSS styling by updating main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext'
+import { LanguageProvider } from './context/LanguageContext'
+import { ThemeProvider } from './context/ThemeContext'
+import { HomePage } from './pages/HomePage'
+import { Toaster } from './components/ui/Toaster'
 import './index.css'
+import './i18n'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider>
+        <LanguageProvider>
+          <AuthProvider>
+            <HomePage />
+            <Toaster />
+          </AuthProvider>
+        </LanguageProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
